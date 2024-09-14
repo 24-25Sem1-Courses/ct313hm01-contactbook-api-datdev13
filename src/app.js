cd5cd4fcafeb1aa99c10c47780f2b299a4902673
datdev13 <datdangthanh17@gmail.com>
2024-09-14 23:10:37 +0700

    Generate OpenAPI Spec from JSDoc

--- src/app.js
+++ src/app.js
@@ -1,30 +1,36 @@
-const express = require('express'); 
-const cors = require('cors'); 
-const JSend = require('./jsend'); 
-const contactsRouter = require('./routes/contacts.router'); 
+const express = require('express');
+const cors = require('cors');
+const JSend = require('./jsend');
+const contactsRouter = require('./routes/contacts.router');
 const {
     resourceNotFound,
     handleError,
 } = require('./controllers/errors.controller');
+const { specs, swaggerUi } = require('./docs/swagger');
 
-const app = express(); 
+const app = express();
 
-app.use(cors()); 
-app.use(express.json()); 
-app.use(express.urlencoded({ extended: true })); 
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => { 
-return res.json(JSend.success()); 
-}); 
+// Serve Swagger UI
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 
+// Serve static files from the 'public' directory
 app.use('/public', express.static('public'));
 
-contactsRouter.setup(app); 
+app.get('/', (req, res) => {
+    return res.json(JSend.success());
+});
 
-// Handle 404 response
+// Setup routes
+contactsRouter.setup(app);
+
+// Handle 404 response for undefined routes
 app.use(resourceNotFound);
 
 // Define error-handling middleware last, after other app.use() and routes calls
 app.use(handleError);
 
-module.exports = app; 
+module.exports = app;
--- src/routes/contacts.router.js
+++ src/routes/contacts.router.js
@@ -1,20 +1,108 @@
-const express = require('express'); 
-const contactsController = require('../controllers/contacts.controller'); 
+const express = require('express');
+const contactsController = require('../controllers/contacts.controller');
 const { methodNotAllowed } = require('../controllers/errors.controller');
 
-const router = express.Router(); 
+const router = express.Router();
 
-module.exports.setup = (app) => { 
-app.use('/api/v1/contacts', router); 
+/**
+ * @swagger
+ * /api/v1/contacts:
+ *   get:
+ *     summary: Get contacts by filter
+ *     description: Get contacts by filter
+ *     parameters:
+ *       - in: query
+ *         name: favorite
+ *         schema:
+ *           type: boolean
+ *         description: Filter by favorite status
+ *       - in: query
+ *         name: name
+ *         schema:
+ *           type: string
+ *         description: Filter by contact name
+ *     tags:
+ *       - contacts
+ *     responses:
+ *       200:
+ *         description: A list of contacts
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   description: The response status
+ *                   enum: [success]
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     contacts:
+ *                       type: array
+ *                       items:
+ *                         $ref: '#/components/schemas/Contact'
+ *   post:
+ *     summary: Create a new contact
+ *     description: Create a new contact
+ *     tags:
+ *       - contacts
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Contact'
+ *     responses:
+ *       201:
+ *         description: Contact created
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Contact'
+ *       400:
+ *         $ref: '#/components/responses/400InvalidRequest'
+ *   delete:
+ *     summary: Delete all contacts
+ *     description: Delete all contacts
+ *     tags:
+ *       - contacts
+ *     responses:
+ *       200:
+ *         description: All contacts deleted
+ *         $ref: '#/components/responses/200NoData'
+ *       500:
+ *         $ref: '#/components/responses/500InternalServerError'
+ *   all:
+ *     summary: Handle unsupported methods
+ *     description: Handle unsupported methods on /api/v1/contacts
+ *     tags:
+ *       - contacts
+ *     responses:
+ *       405:
+ *         description: Method not allowed
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   description: The response status
+ *                   enum: [fail]
+ *                 message:
+ *                   type: string
+ *                   description: Error message
+ */
 
-router.get('/', contactsController.getContactsByFilter); 
-router.post('/', contactsController.createContact); 
-router.delete('/', contactsController.deleteAllContacts); 
+router.get('/', contactsController.getContactsByFilter);
+router.post('/', contactsController.createContact);
+router.delete('/', contactsController.deleteAllContacts);
 router.all('/', methodNotAllowed);
 
-router.get('/:id', contactsController.getContact); 
-router.put('/:id', contactsController.updateContact); 
-router.delete('/:id', contactsController.deleteContact); 
+router.get('/:id', contactsController.getContact);
+router.put('/:id', contactsController.updateContact); // Add this line for PUT /api/v1/contacts/{id}
+router.delete('/:id', contactsController.deleteContact);
 router.all('/:id', methodNotAllowed);
 
-}; 
+module.exports = { setup: (app) => app.use('/api/v1/contacts', router) };
