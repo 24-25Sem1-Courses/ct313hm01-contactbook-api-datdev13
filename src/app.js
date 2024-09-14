44930bcc61a3bc55df387b7c38ad0ff69be2594f
datdev13 <datdangthanh17@gmail.com>
2024-09-14 21:56:32 +0700

    Serve static files

--- src/app.js
+++ src/app.js
@@ -12,9 +12,13 @@ const app = express();
 app.use(cors()); 
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: true })); 
+
 app.get('/', (req, res) => { 
 return res.json(JSend.success()); 
 }); 
+
+app.use('/public', express.static('public'));
+
 contactsRouter.setup(app); 
 
 // Handle 404 response
